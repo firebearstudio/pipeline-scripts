22fd693516185a63f322b696b2394dcf1ac3cab3
skapitan <51701877+skapitan@users.noreply.github.com>
2019-06-15 14:31:08 +0300

    Removed upgrade and compile.
    Magento upgrade and compile are runs by a separate step in bitbucket-pipelines.yml

--- static-tests.sh
+++ static-tests.sh
@@ -5,6 +5,4 @@ git fetch
 git checkout origin/$1
 cd ../../../../
 
-bin/magento setup:upgrade &&
-bin/magento setup:di:compile &&
 vendor/bin/phpstan analyse --no-progress -c /home/owner/scripts/phpstan.neon app/code/Firebear/ImportExport
