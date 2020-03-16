9b35ec3d606300830fb6cefa57ddbbdd5b12f42a
Taras K <somework@gmail.com>
2020-03-17 00:13:04 +0200

    Add --severity=1 param to phpcs

--- codestyle.sh
+++ codestyle.sh
@@ -8,5 +8,5 @@ cd ../../../../
 module=app/code/Firebear/ImportExport
 codesniffer=$module/dev/codesniffer
 
-vendor/bin/phpcs -p --standard=$codesniffer/ruleset.xml $module &&
-vendor/bin/phpcs -p --standard=$codesniffer/ruleset_addon.xml $module
+vendor/bin/phpcs -p --severity=1 --standard=$codesniffer/ruleset.xml $module &&
+vendor/bin/phpcs -p --severity=1 --standard=$codesniffer/ruleset_addon.xml $module
